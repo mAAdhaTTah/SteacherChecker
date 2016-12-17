cd9fd11f5c621e3badfda311fe5dd29a842cfe18
Jordan Kelly <jkelly2@aoiths.org>
2016-12-17 21:39:56 +0000

    Fix the login page

--- Login.js
+++ Login.js
@@ -1,15 +1,36 @@
 /* global $*/
+// user object used below comes from X-creds.js
 $(function() {
     $(".btn3").click(function() {
         var name= $("#username").val();
         var password= $("#password").val();
         
         if (name !== user.name) {
-            alert("wrong name")
+            alert("wrong name");
         } else if (password !== user.password) {
-            alert("wrong password")
+            alert("wrong password");
         } else {
             if (user.type === 'teacher') {
+                window.open('teacherpage.html', '_self');
+            } else {
+                window.open('student.html', '_self');
+            }
+            
+        }
+    });
+});
+
+$(function() {
+    $(".btn3").click(function() {
+        var name= $("#username").val();
+        var password= $("#password").val();
+        
+        if (name !== user2.name) {
+            alert("wrong name");
+        } else if (password !== user.password) {
+            alert("wrong password");
+        } else {
+            if (user2.type === 'teacher') {
                 window.open('teacherpage.html', '_self');
             } else {
                 window.open('student.html', '_self');
